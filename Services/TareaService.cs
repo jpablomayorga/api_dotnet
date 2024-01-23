31b894f014ee209972bcca8d0295332e1e549885
Juan Pablo Mayorga <jpablomayorga@gmail.com>
2024-01-23 14:43:28 -0500

    create controllers

--- Services/TareaService.cs
+++ Services/TareaService.cs
@@ -1,50 +1,50 @@
-using webapi;
 using webapi.Models;
 
 namespace webapi.Services;
-public class TareaService: ITareaService
-{
-    readonly TareasContext context;
 
-    public TareaService(TareasContext dbcontext)
-    {
-        context = dbcontext;
+public class TareaService : ITareaService
+{
+    TareasContext context;
+    public TareaService(TareasContext dbContext) {
+        context = dbContext;
     }
     public IEnumerable<Tarea> Get(){
         return context.Tareas;
     }
-    /*public async Task Save(Categoria categoria)
-    {
-        context.Add(categoria);
+
+    public async Task Save(Tarea tarea) {
+        context.Add(tarea);
         await context.SaveChangesAsync();
     }
-    public async Task Update(Guid id, Categoria categoria)
-    {
-        var categoriaActual = context.Categorias.Find(id);
-        if(categoriaActual != null)
-        {
-            categoriaActual.Nombre = categoria.Nombre;
-            categoriaActual.Descripcion = categoria.Descripcion;
-            categoriaActual.Peso = categoria.Peso;
+
+    public async Task Update(Guid id, Tarea tarea) {
+        var tareaActual = context.Tareas.Find(id);
+        if (tareaActual != null) {
+            tareaActual.CategoriaId = tarea.CategoriaId;
+            tareaActual.Titulo = tarea.Titulo;
+            tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+            tareaActual.Descripcion = tarea.Descripcion;
+
             await context.SaveChangesAsync();
-        }             
+        }
+
     }
-     public async Task Delete(Guid id)
-    {
-        var categoriaActual = context.Categorias.Find(id);
-        if(categoriaActual != null)
-        {
-            context.Remove(categoriaActual);
+
+    public async Task Delete(Guid id) {
+        var tareaActual = context.Tareas.Find(id);
+        if (tareaActual != null) {
+            context.Remove(tareaActual);
             await context.SaveChangesAsync();
-        }     
+        }
         
-    }*/
+    }
 }
 
 public interface ITareaService
 {
     IEnumerable<Tarea> Get();
-    /*Task Save(Categoria categoria);
-    Task Update(Guid id, Categoria categoria);
-    Task Delete(Guid id);*/
+    Task Save(Tarea tarea);
+    Task Update(Guid id, Tarea tarea);
+    Task Delete(Guid id);
+
 }
