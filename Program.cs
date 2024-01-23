93086bb342f2bbfc3d991fdbfa5125660bbad33f
Juan Pablo Mayorga <jpablomayorga@gmail.com>
2024-01-22 23:08:10 -0500

    inyeccion de dependencias de los servicios en program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using webapi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<ITareaService, TareaService>();
 
 var app = builder.Build();
 
